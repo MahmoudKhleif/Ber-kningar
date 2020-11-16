15d4f90c725a4ba528198af4bddeef070e797954
unknown <mahmoud89ah@gmail.com>
2020-11-16 03:00:53 +0100

    initial commit

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {  
+        public static void mahmoud()
+        {
+            Console.WriteLine("her is my class : ");
+        }
+       public static void Mymethod()
+        {
+            Console.WriteLine("welcome to my first class");
+
+
+        }
+    }
+}
